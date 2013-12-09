f081e7cad33885a4eeb7d7753b570893d8affcf1
Robert <github@theperfectfuel.com>
2013-12-09 11:08:55 -0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Pinstruments::Application.config.secret_key_base = '12e79c4c394429850535fd082335b4a7f12fb89ec49c560ce40d516f3ba545a0bf0e69dac6eb45fba14ee28a5aadfebe112075e1f4d2624677dc5b26fe3a590f'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Pinstruments::Application.config.session_store :cookie_store, key: '_pinstruments_session'
