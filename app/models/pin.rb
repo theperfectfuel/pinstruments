50b4742954cbc587b376706ff8f7e842d7b0b615
Robert <github@theperfectfuel.com>
2013-12-13 17:08:08 -0800

    Added remote url images

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -3,5 +3,10 @@ class Pin < ActiveRecord::Base
 
 	has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" }
 
+	def image_remote_url=(url_value)
+		self.image = URI.parse(url_value) unless url_value.blank?
+		super
+	end
+
 	validates :image, presence: true
 end
