c9a680a9f0d037f62c5d31580aa78c3f31343d88
Robert <github@theperfectfuel.com>
2013-12-13 18:41:12 -0800

    Changed images sizes available

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -1,7 +1,7 @@
 class Pin < ActiveRecord::Base
 	belongs_to :user
 
-	has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" }
+	has_attached_file :image, :styles => { :large => "600x600>", :medium => "300x300>", :thumb => "100x100>" }
 
 	def image_remote_url=(url_value)
 		self.image = URI.parse(url_value) unless url_value.blank?
